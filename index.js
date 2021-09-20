c42ec37993c5cd8d88cfe3b46622ce3cdc0622e3
Luciana Sugeta <lulusugeta@gmail.com>
2021-09-20 12:25:59 -0400

    commit tree 6ab92cdb7c5cd1a26dc347f4d19e5d9133f3c46a

---
+++ index.js
@@ -0,0 +1,25 @@
+// Scooter with year, color, and model properties
+class Scooter {
+    constructor(year, color, model) {
+        this.year = year;
+        this.color = color;
+        this.model = model;
+    };
+};
+
+// Driver with name, age, and experience properties
+class Driver {
+    constructor(name, age, experience) {
+        this.name = name;
+        this.age = age;
+        this.experience = experience;
+    };
+};
+
+// PickupLocation with address and city properties
+class PickupLocation {
+    constructor(address, city) {
+        this.address = address;
+        this.city = city;
+    };
+};
